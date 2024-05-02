94a9f34ed1cd5da116a5fe4e01d6f89ab8e3fdd6
Jeff AliG <aligwekwe2000j@gmail.com>
2024-05-02 15:59:39 +0100

    page

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -3,25 +3,31 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { MapPin, Smartphone } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 export default function ContactPage() {
   return (
     <div>
       <MaxWidthWrapper className="">
-        <div className="flex">
-          <div className="flex flex-col p-7 gap-5 w-full md:w-1/2">
-            <div className="">Contact Us</div>
+        <div className="flex ">
+          <div className="flex flex-col p-7 gap-10 w-full md:w-1/2">
+            <div className="text-4xl">Contact Us</div>
             <div className="">
               <MapPin />
-              121 Rock Sreet, 21 Avenue, New York, NY 92103-9000
+              located at 3rd Floor Polystar Building, Marwa, Lekki.
             </div>
             <div className="">
               <Smartphone />1 (234) 567-891 1 (234) 987-654
             </div>
-            <div className="">
-              We are looking forward to start a project with you!
-            </div>
+            <div className="">We create and improve web & mobile products</div>
+
+            <Image
+              src="/christina.jpg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
           </div>
 
           <div className="flex flex-col gap-5 p-7 w-full md:w-1/2">
@@ -52,6 +58,10 @@ export default function ContactPage() {
             </div>
 
             <Button>Submit</Button>
+            <div className="">
+              Partner with us and strategize for the future, We are looking
+              forward to start a project with you!
+            </div>
           </div>
         </div>
       </MaxWidthWrapper>
