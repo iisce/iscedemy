8cba5870085df35838ee5ebd55939cc40919e4f9
LeoDivine <divineonyi2004@gmail.com>
2024-05-06 19:21:23 +0100

    component inclusion

---
+++ app/(root)/home/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export default function Home() {
+  return (
+    <>
+      <div className="w-full flex">
+        <div className="w-[50%] px-[50px] my-[50px] ">
+          <h3 className="text-[50px] font-bold ">
+            <span className=" font-normal">❝</span>Creativity is INTELLIGENCE
+            having fun.<span className="font-normal">❞</span>
+          </h3>
+          <i>-Albert Einstein.</i>
+          <br />
+          <Link href="">
+            {" "}
+            <Button className="mt-[10px]">
+              Apply now <ArrowRight className="w-5 h-4 " />{" "}
+            </Button>{" "}
+          </Link>
+        </div>
+        <div className="w-[40%]">
+          <Image
+            className=" mt-[26.5px] "
+            src="/right.png"
+            height="1000"
+            width="1000"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+}
