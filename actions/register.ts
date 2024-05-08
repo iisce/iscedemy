407b18ee280a64b636999702ee33509648a3c6a6
oyeniran Ayobami Paul <ap.oyeniran@gmail.com>
2024-05-08 14:33:15 +0100

    upd: fix errors

--- actions/register.ts
+++ actions/register.ts
@@ -1,33 +1,31 @@
-'use server'
+'use server';
 import { RegisterSchema } from '@/schemas';
 import * as z from 'zod';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { db } from '@/lib/db';
 import getUserByEmail from '@/data/user';
 
-
 export const Register = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
+	const validatedFields = RegisterSchema.safeParse(values);
 
-    if (!validatedFields.success) {
-        return { error: 'Invalid fields!'};
-    }
-    
-    const { email, name, password } = validatedFields.data;
-    const hashedPassword = await bcrypt.hash(password, 10)
+	if (!validatedFields.success) {
+		return { error: 'Invalid fields!' };
+	}
 
-    const existingUser = await getUserByEmail(email);
-    
-    if (existingUser) {
-        return { error: "Email already in use!" };
-    }
-    await db.user.create({
-        data: {
-            name,
-            email,
-            password: hashedPassword,
-        }
-    })
-    return { success: 'Account created!'};
-}
+	const { email, name, password } = validatedFields.data;
+	const hashedPassword = await bcrypt.hash(password, 10);
 
+	const existingUser = await getUserByEmail(email);
+
+	if (existingUser) {
+		return { error: 'Email already in use!' };
+	}
+	await db.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPassword,
+		},
+	});
+	return { success: 'Account created!' };
+};
