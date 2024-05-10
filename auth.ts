d2b666ab7ae9eb55a9478920424cace4447da643
fusco <emekaignatius5@gmail.com>
2024-05-10 16:32:49 +0100

    fix: fixed error after rebase

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from '@auth/prisma-adapter'
+// import { PrismaAdapter } from '@auth/prisma-adapter'
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
 import { getUserById } from "./data/user";
