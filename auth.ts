be18c569adc2a5af79584d818eaaa599d84e11db
oyeniran Ayobami Paul <ap.oyeniran@gmail.com>
2024-05-10 17:53:41 +0100

    fix: fixed resend mailing issue

--- auth.ts
+++ auth.ts
@@ -1,54 +1,50 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from '@auth/prisma-adapter'
-// import { PrismaAdapter } from '@auth/prisma-adapter'
-import authConfig from "./auth.config";
-import { db } from "./lib/db";
-import { getUserById } from "./data/user";
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import authConfig from './auth.config';
+import { db } from './lib/db';
+import { getUserById } from './data/user';
 
+export const {
+	auth,
+	signIn,
+	signOut,
+	handlers: { GET, POST },
+} = NextAuth({
+	pages: {
+		signIn: '/login',
+		error: '/error',
+	},
+	events: {
+		async linkAccount({ user }) {
+			await db.user.update({
+				where: { id: user.id },
+				data: { emailVerified: new Date() },
+			});
+		},
+	},
+	callbacks: {
+		async signIn({ user, account }) {
+			if (account?.provider !== 'credentials') return true;
 
-export const { 
-  auth, 
-  signIn, 
-  signOut,
-  handlers: {GET, POST}, 
- } = NextAuth({
-  pages: {
-    signIn: "/login",
-    error: "/error"
-  },
-  events: {
-    async linkAccount({ user }){
-      await db.user.update({
-        where: { id: user.id },
-        data: {  emailVerified: new Date()  }
-      })
-    }
-  },
-  callbacks: {
-    async signIn({ user, account }) {
-      if (account?.provider !== "credentials") 
-      return true;
+			const existingUser = await getUserById(user.id!);
 
-      const existingUser = await getUserById(user.id!);
+			if (!existingUser?.emailVerified) return false;
 
-      if (!existingUser?.emailVerified)
-      return false
+			return true;
+		},
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+			return session;
+		},
 
-      return true;
-    },
-    async  session({ token, session }){
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
-      }
-      return session;
-    },
-
-    async jwt({ token }) {
-      console.log(token)
-      return token; 
-    }
-  },
-  adapter: PrismaAdapter(db),
-  session: { strategy: "jwt" },
-  ...authConfig
+		async jwt({ token }) {
+			console.log(token);
+			return token;
+		},
+	},
+	adapter: PrismaAdapter(db),
+	session: { strategy: 'jwt' },
+	...authConfig,
 });
--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,31 +1,36 @@
-"use client";
-import { COURSEITEM } from "@/lib/consts";
-import { CircleRightArrow } from "@/lib/icons";
-import Link from "next/link";
-import { useState } from "react";
-import CourseItem from "../pages/courses/courseitem";
-import SignOutButton from "../ui/sign-out";
+'use client';
+import { COURSEITEM } from '@/lib/consts';
+import { CircleRightArrow } from '@/lib/icons';
+import Link from 'next/link';
+import { useState } from 'react';
+import CourseItem from '../pages/courses/courseitem';
+import SignOutButton from '../ui/sign-out';
 
 export default function SideBar() {
-  
-  const [click, setClicked] = useState("h-0");
-  return (
-    <div className="hidden md:flex flex-col gap-4 justify-between md:text-left md:h-[calc(100%-80px)] md:w-[16%] ">
-      <div>
-      <div className="flex pb-[10px] items-center gap-3">
-        <h2 className="font-bold">All Courses</h2>
-        <Link href='/courses'><span>{CircleRightArrow}</span></Link>
-      </div>
-      <hr />
-      <ul className="text-[13px] text-[#504f4f] flex gap-3 flex-col pt-[20px] ">
-        {COURSEITEM.map((course, k) => (
-          <CourseItem key={k} link={course.link} name={course.name} />
-        ))}
-      </ul>
-      </div>
-      <div className="">
-      <SignOutButton />
-      </div>
-    </div>
-  );
+	const [click, setClicked] = useState('h-0');
+	return (
+		<div className='hidden md:flex flex-col gap-4 justify-between md:text-left md:h-[calc(100%-80px)] md:w-[16%] '>
+			<div>
+				<div className='flex pb-[10px] items-center gap-3'>
+					<h2 className='font-bold'>All Courses</h2>
+					<Link href='/courses'>
+						<span>{CircleRightArrow}</span>
+					</Link>
+				</div>
+				<hr />
+				<ul className='text-[13px] text-[#504f4f] flex gap-3 flex-col pt-[20px] '>
+					{COURSEITEM.map((course, k) => (
+						<CourseItem
+							key={k}
+							link={course.link}
+							name={course.name}
+						/>
+					))}
+				</ul>
+			</div>
+			<div className=''>
+				<SignOutButton />
+			</div>
+		</div>
+	);
 }
