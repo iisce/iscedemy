4619370a4582a0f13e250d8a6d90db2961c01f26
oyeniran Ayobami Paul <ap.oyeniran@gmail.com>
2024-06-18 14:11:35 +0100

    fix: :bug: fixed env file

--- actions/paystack.ts
+++ actions/paystack.ts
@@ -1,8 +1,7 @@
 'use server';
 
-import { db } from "@/lib/db";
-import { headers } from "next/headers";
-
+import { db } from '@/lib/db';
+import { headers } from 'next/headers';
 
 export const createTransaction = async (opts: { payload: any }) => {
 	try {
@@ -11,7 +10,7 @@ export const createTransaction = async (opts: { payload: any }) => {
 			{
 				method: 'POST',
 				headers: {
-					Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY_TEST}`, 
+					Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
 					'Content-Type': 'application/json',
 				},
 				body: JSON.stringify(opts.payload),
@@ -19,11 +18,13 @@ export const createTransaction = async (opts: { payload: any }) => {
 		);
 
 		const paymentResponse = await response.json();
-    console.log("Transaction Initialization Response:", paymentResponse);
+		console.log('Transaction Initialization Response:', paymentResponse);
 
-    if (!paymentResponse?.status) {
-      return { error: 'An error occurred while initializing the payment.' };
-    }
+		if (!paymentResponse?.status) {
+			return {
+				error: 'An error occurred while initializing the payment.',
+			};
+		}
 
 		// Redirect user to payment page
 		const data = paymentResponse.data;
@@ -36,45 +37,40 @@ export const createTransaction = async (opts: { payload: any }) => {
 	}
 };
 
+export async function verifyTransaction(transactionReference: string) {
+	try {
+		const response = await fetch(
+			`https://api.paystack.co/transaction/verify/${transactionReference}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+				},
+			}
+		);
+		const paymentResponse = await response.json();
+		console.log('Paystack response:', paymentResponse);
 
-export async function verifyTransaction( transactionReference: string)
- {
-  try {
-    const response = await fetch(
-      `https://api.paystack.co/transaction/verify/${transactionReference}`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY_TEST}`, 
-        },
-      }
-    );
-    const paymentResponse = await response.json();
-    console.log("Paystack response:", paymentResponse);
+		if (
+			!paymentResponse?.status ||
+			paymentResponse.data.status !== 'success'
+		) {
+			return { error: 'Transaction failed or could not be verified.' };
+		}
 
+		const transaction = await db.coursePayment.findFirst({
+			where: { transactionId: transactionReference },
+		});
 
-    if (!paymentResponse?.status || paymentResponse.data.status !== 'success') {
-      return {error: "Transaction failed or could not be verified."}; 
-	  }
-    
-
-    const transaction = await db.coursePayment.findFirst({
-      where: {transactionId : transactionReference}
-    });
-
-    if (!transaction) {
-      return {error: "Transaction not found"}
-    }
-    return {success: "Transaction Verified"};
-
-
-
-  } catch (error) {
-    console.error("Error verifying Paystack transaction:", error);
-    if (error instanceof Error) {
-      return {error: error.message };
-    }
-    return {error: "Error verifying transaction." };
-  }
+		if (!transaction) {
+			return { error: 'Transaction not found' };
+		}
+		return { success: 'Transaction Verified' };
+	} catch (error) {
+		console.error('Error verifying Paystack transaction:', error);
+		if (error instanceof Error) {
+			return { error: error.message };
+		}
+		return { error: 'Error verifying transaction.' };
+	}
 }
-
