8d7784009a462fc3bb71ca470bb8017241c7b17c
emeka <emekaignatius5@gmail.com>
2024-08-10 18:14:53 +0100

    chore: ran build

--- components/pages/courses/admin-course-list.tsx
+++ components/pages/courses/admin-course-list.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
 import { AdminCourseCard } from '../../../components/pages/courses/admin-course-card'
-// import { Button } from "@/components/ui/button";
-// import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from '../../../components/ui/button';
 import {Checkbox } from '../../../components/ui/checkbox';
 import {
