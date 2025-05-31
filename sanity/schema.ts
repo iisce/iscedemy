a1a2bfae357242e73abdc9a3aafe11b26e0c51c6
fusco <emekaignatius5@gmail.com>
2025-05-31 22:47:16 +0100

    fix: fixed the undefined author issue in sanity studio and removed the session in the project section component to prevent users from being redirected to login even when they are already logged in

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { post } from "./blog-schemas/post";
 import { tag } from "./blog-schemas/tag";
+import { author } from "./blog-schemas/author";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-     types: [post, tag],
+     types: [post, tag, author],
 };
